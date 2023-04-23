68a3474787f21af801cdaf59ae96c3a1aef2cd88
black-sheep <shivamguptanitw.com>
2023-04-23 15:26:07 +0530

    Commit 9: User Schema created

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -1,2 +1,53 @@
 const mongoose = require('mongoose')
 
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+    },
+    lastName:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone:{
+        type: String,
+        min: 10,
+        max: 10,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+        min: 6
+    },
+    jobProfile:{
+        type: String,
+        required: true,
+    },
+    companyName:{
+        type: String,
+        required: true,
+    },
+    skills:{
+        type: String,
+        required: true,
+    },
+    linkedIn:{
+        type: String,
+        required: true,
+    },
+    gitHub:{
+        type: String,
+        required: true,
+    },
+},{
+    timestamps: true
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
